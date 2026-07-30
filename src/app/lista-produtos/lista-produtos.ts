46e16183d63fd0ee8f6a18453cd26c917baaa8a5
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-07-30 20:01:37 -0300

    Adicionado a instancia e a funcao para limpar o formulario

--- src/app/lista-produtos/lista-produtos.ts
+++ src/app/lista-produtos/lista-produtos.ts
@@ -11,21 +11,35 @@ import { Item } from './item';
 export class ListaProdutos {
   descricao_produto? : string
   valor_unitario? : number
+  listaItens : Item[] = []
 
   addItem(){
     console.log(this.descricao_produto, `<->`, this.valor_unitario)
 
-    //Instancia da classe item passando os parametros do construtor
+    //INSTANCIA DA CLASSE ITEM PASSANDO OS PARAMETROS DO CONSTRUTOR
     //let item = new Item(this.descricao_produto, this.valor_unitario)
 
+    //INSTANCIA DA CLASSE ITEM (CRIANDO O OBJETO item)
     let item = new Item()
+    //ARMENGO NAO FAZER
+    item.idProduto = this.listaItens.length + 1
     item.descricaoProduto = this.descricao_produto
     item.valorUnitario = this.valor_unitario
 
-    
+    //ADICIONANDO OBJETO item AO ARRAY listaItens
+    this.listaItens.push(item)
+
+    //LIMPANDO AS PROPRIEDADES DA CLASSE
+    this.descricao_produto = ''
+    this.valor_unitario = 0.0
+
+
   }
 
   btnLimpar(){
-
+    this.descricao_produto = ''
+    this.valor_unitario = 0.0
   }
+
+  
 }
