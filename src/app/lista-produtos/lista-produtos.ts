6c64eaf07fb4a556111d85c8e1648dddd1fd984a
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-07-30 19:30:28 -0300

    Feita a importação de formsmodule e adicionado o codigo class ListaProdutos

--- src/app/lista-produtos/lista-produtos.ts
+++ src/app/lista-produtos/lista-produtos.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Item } from './item';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
-export class ListaProdutos {}
+export class ListaProdutos {
+  descricao_produto? : string
+  valor_unitario? : number
+
+  addItem(){
+    console.log(this.descricao_produto, `<->`, this.valor_unitario)
+
+    //Instancia da classe item passando os parametros do construtor
+    //let item = new Item(this.descricao_produto, this.valor_unitario)
+
+    let item = new Item()
+    item.descricaoProduto = this.descricao_produto
+    item.valorUnitario = this.valor_unitario
+
+    
+  }
+
+  btnLimpar(){
+
+  }
+}
