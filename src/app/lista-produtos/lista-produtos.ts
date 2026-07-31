3ed0ca4048361111c3f9db7e46f9555cdc4166fd
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-07-30 21:16:44 -0300

    correções no codigo

--- src/app/lista-produtos/lista-produtos.ts
+++ src/app/lista-produtos/lista-produtos.ts
@@ -9,20 +9,22 @@ import { Item } from './item';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  descricao_produto? : string
-  valor_unitario? : number
+  descricao_produto: string = ''
+  valor_unitario: number = 0.0
   listaItens : Item[] = []
 
   addItem(){
-    console.log(this.descricao_produto, `<->`, this.valor_unitario)
+    //console.log(this.descricao_produto, `<->`, this.valor_unitario)
 
     //INSTANCIA DA CLASSE ITEM PASSANDO OS PARAMETROS DO CONSTRUTOR
     //let item = new Item(this.descricao_produto, this.valor_unitario)
 
     //INSTANCIA DA CLASSE ITEM (CRIANDO O OBJETO item)
     let item = new Item()
-    //ARMENGO NAO FAZER
+
+    //ARMENGO
     item.idProduto = this.listaItens.length + 1
+
     item.descricaoProduto = this.descricao_produto
     item.valorUnitario = this.valor_unitario
 
@@ -36,10 +38,6 @@ export class ListaProdutos {
 
   }
 
-  btnLimpar(){
-    this.descricao_produto = ''
-    this.valor_unitario = 0.0
-  }
 
-  
+
 }
